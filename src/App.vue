c47ac30baccbf98efd1b998f82cc3884845e2a92
xuanwushijie <1091237632@qq.com>
2018-07-02 20:48:08 +0800

    slot 缓存

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div class="todo-container">
     <div class="todo-wrap">
       <todo-header :addTodo="addTodo"/>
-     <!-- <List :todos="todos" :deleteTodo="deleteTodo"/>-->
+      <!-- <List :todos="todos" :deleteTodo="deleteTodo"/>-->
       <List :todos="todos"/>
-      <todo-footer :todos="todos" :deleteComputed="deleteComputed" :selectAll="selectAll"/>
+      <todo-footer>
+        <input type="checkbox" v-model="isCheck" slot="check"/>
+        <span slot="count">已完成{{completedCount}}  / 全部{{todos.length}} </span>
+        <button class="btn btn-danger" v-show="completedCount" @click="deleteComputed" slot="btn">清除已完成任务</button>
+      </todo-footer>
     </div>
   </div>
 </template>
@@ -14,44 +18,67 @@
   import Header from './components/Header.vue'
   import List from './components/List.vue'
   import Footer from './components/Footer.vue'
+  import storageUtil from './util/storageUtil'
 
   export default { // 配置对象
 
     data () {
       return {
-        todos: [
-          {title: '吃饭', completed: false},
-          {title: '睡觉', completed: true},
-          {title: '打代码', completed: false}
-        ]
+        todos: storageUtil.readTodos()
       }
     },
+    computed: {
+      completedCount () { // 完成的数量
+        return this.todos.reduce((preTotal, todo) => preTotal + (todo.completed?1:0), 0)
+      },
 
+      isCheck: {
+        get () {
+          return this.todos.length===this.completedCount && this.completedCount>0
+        },
+
+        set (value) {
+          // 进行全选或全不选
+          this.selectAll(value)
+        }
+      }
+    },
     components: {
-      TodoHeader:Header,
+      TodoHeader: Header,
       List,
       TodoFooter: Footer
     },
-    mounted () {
+    mounted() {
       // 订阅消息(deleteTodo)
       PubSub.subscribe('deleteTodo', (msg, index) => {
         this.deleteTodo(index)
       })
     },
     methods: {
-      addTodo (todo) {
+      addTodo(todo) {
         this.todos.unshift(todo)
       },
-      deleteTodo (index) {
+      deleteTodo(index) {
         this.todos.splice(index, 1)
       },
-      deleteComputed () {
+      deleteComputed() {
         this.todos = this.todos.filter(todo => !todo.completed)
       },
 
       // 全选或全不选
-      selectAll (check) {
+      selectAll(check) {
         this.todos.forEach(todo => todo.completed = check)
+      }
+    },
+    watch: {
+      todos: {
+        deep: true, // 深度监视
+        /*handler: function (val) { // todos发生了改变
+          // 保存todos(json)到local
+          //window.localStorage.setItem('todos_key', JSON.stringify(val))
+          storageUtil.saveTodos(val)
+        }*/
+        handler: storageUtil.saveTodos
       }
     }
   }
@@ -62,6 +89,7 @@
     width: 600px;
     margin: 0 auto;
   }
+
   .todo-container .todo-wrap {
     padding: 10px;
     border: 1px solid #ddd;
